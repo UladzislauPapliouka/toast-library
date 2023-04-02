1854ad8aea812e9d8e5f9e8c3fc2d923a641e6c1
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2023-04-02 14:39:57 +0300

    refactor: refactor .stories

--- src/stories/Toast.stories.jsx
+++ src/stories/Toast.stories.jsx
@@ -3,41 +3,41 @@ import React from 'react';
 import Toast from '@components/Toast';
 
 export default {
-    title: 'Example/Toast',
-    component: Toast,
-    argTypes: {
-        type: {
-            defaultValue: 'info',
-            options: ['info', 'warning', 'error', 'success'],
-            control: { type: 'select' },
-        },
-        toastTitle: {
-            defaultValue: 'Test',
-        },
-        toastDescription: {
-            defaultValue: 'Description',
-        },
-        animationName: {
-            defaultValue: 'slide',
-        },
+  title: 'Example/Toast',
+  component: Toast,
+  argTypes: {
+    type: {
+      defaultValue: 'info',
+      options: ['info', 'warning', 'error', 'success'],
+      control: { type: 'select' },
     },
+    toastTitle: {
+      defaultValue: 'Test',
+    },
+    toastDescription: {
+      defaultValue: 'Description',
+    },
+    animationName: {
+      defaultValue: 'slide',
+    },
+  },
 };
 
 function Template(args) {
-    return <Toast {...args} />;
+  return <Toast {...args} />;
 }
 
 export const Default = Template.bind({});
 Default.args = {};
 export const Warning = Template.bind({});
 Warning.args = {
-    type: 'warning',
+  type: 'warning',
 };
 export const Error = Template.bind({});
 Error.args = {
-    type: 'error',
+  type: 'error',
 };
 export const Success = Template.bind({});
 Success.args = {
-    type: 'success',
+  type: 'success',
 };
--- src/stories/ToastContainer.stories.jsx
+++ src/stories/ToastContainer.stories.jsx
@@ -6,75 +6,75 @@ import Button from '../components/Button/Button';
 import Singleton from '../constants/singleton';
 
 export default {
-    title: 'Example/ToastContainer',
-    component: ToastContainer,
-    argTypes: {
-        type: {
-            defaultValue: 'info',
-            options: ['info', 'warning', 'error', 'success'],
-            control: { type: 'select' },
-        },
-        toastTitle: {
-            defaultValue: 'Test',
-            control: { type: 'text' },
-        },
-        toastDescription: {
-            defaultValue: 'Description',
-            control: { type: 'text' },
-        },
-        animationName: {
-            defaultValue: 'opacity',
-            options: ['opacity', 'slide'],
-            control: { type: 'select' },
-        },
-        toastPosition: {
-            defaultValue: 'top-left',
-            options: ['top-left', 'top-right', 'bottom-right', 'bottom-left'],
-            control: { type: 'select' },
-        },
-        slideDirection: {
-            defaultValue: 'top',
-            options: ['left', 'right', 'bottom', 'top'],
-            control: { type: 'select' },
-        },
-        duration: {
-            defaultValue: 3000,
-            control: { type: 'number' },
-        },
+  title: 'Example/ToastContainer',
+  component: ToastContainer,
+  argTypes: {
+    type: {
+      defaultValue: 'info',
+      options: ['info', 'warning', 'error', 'success'],
+      control: { type: 'select' },
     },
+    toastTitle: {
+      defaultValue: 'Test',
+      control: { type: 'text' },
+    },
+    toastDescription: {
+      defaultValue: 'Description',
+      control: { type: 'text' },
+    },
+    animationName: {
+      defaultValue: 'opacity',
+      options: ['opacity', 'slide'],
+      control: { type: 'select' },
+    },
+    toastPosition: {
+      defaultValue: 'top-left',
+      options: ['top-left', 'top-right', 'bottom-right', 'bottom-left'],
+      control: { type: 'select' },
+    },
+    slideDirection: {
+      defaultValue: 'top',
+      options: ['left', 'right', 'bottom', 'top'],
+      control: { type: 'select' },
+    },
+    duration: {
+      defaultValue: 3000,
+      control: { type: 'number' },
+    },
+  },
 };
 
 function Template({
-    type,
-    toastTitle,
-    toastDescription,
-    animationName,
-    toastPosition,
-    slideDirection,
-    duration,
-    ...args
+  type,
+  toastTitle,
+  toastDescription,
+  animationName,
+  toastPosition,
+  slideDirection,
+  duration,
+  ...args
 }) {
-    return (
-        <div>
-            <ToastContainer />
-            <Button
-                onClick={() =>
-                    Singleton.getInstance().createToast({
-                        title: toastTitle,
-                        position: toastPosition,
-                        type,
-                        animationName,
-                        description: toastDescription,
-                        slideDirection,
-                        duration,
-                    })
-                }
-                data-cy="createToast"
-            >
-                Create toast
-            </Button>
-        </div>
-    );
+  return (
+    <div>
+      <ToastContainer />
+      <Button
+        onClick={() =>
+          Singleton.getInstance().createToast({
+            title: toastTitle,
+            position: toastPosition,
+            type,
+            animationName,
+            description: toastDescription,
+            slideDirection,
+            duration,
+          })
+        }
+        data-cy="createToast"
+      >
+        Create toast
+      </Button>
+    </div>
+  );
 }
 
 export const Default = Template.bind({});
